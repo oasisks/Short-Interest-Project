d11f7b75e12904b734f17ea8c279de59d1d5c04e
unknown <oasis@LAPTOP-NPA63NIP.mit.edu>
2022-08-14 19:46:21 -0400

    Updated Universe

--- src/Universe.py
+++ src/Universe.py
@@ -1,2 +1,17 @@
+from Stock import Stock
+"""
+The universe class will contain all the stocks that exists within a particular universe.
+The universe can be specified using specific specifications
+"""
+
+
 class Universe:
-    pass
+    def __init__(self, volume, sector, price_range, exchanges=("nasdaq", "amex", "nyse")):
+        self.volume = volume
+        self.sector = sector
+        self.price_range = price_range
+        self.exchanges = exchanges
+        self.stocks = self._generate_stocks()
+
+    def _generate_stocks(self) -> [Stock]:
+        pass
