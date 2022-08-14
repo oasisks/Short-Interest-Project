a7be71bb96f18da939a5c4582d94a90abae7c573
unknown <oasis@LAPTOP-NPA63NIP.mit.edu>
2022-08-14 19:35:07 -0400

    Added some basic functionalities.

---
+++ src/Stock.py
@@ -0,0 +1,11 @@
+class Stock:
+    """
+    The stock class will contain price points, volume, and short interest
+    """
+    def __init__(self, ticker):
+        self.ticker = ticker
+
+
+
+
+
---
+++ src/main.py
@@ -0,0 +1,3 @@
+from Stock import Stock
+
+
